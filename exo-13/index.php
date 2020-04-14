97800e066fd29b8fbb4f59ac1b95635628676714
JJonathan <jolivald.jonathan@gmail.com>
2020-04-14 16:24:44 +0200

    install dependencies

---
+++ exo-13/index.php
@@ -0,0 +1,10 @@
+<?php
+
+// utilise l'autoload fournit par composer
+$loader = require __DIR__.'/vendor/autoload.php';
+
+// ajoute notre namespace à l'autoload
+$loader->addPsr4('Jonathan\\classes\\', __DIR__.'/classes/');
+$loader->addPsr4('Jonathan\\controllers\\', __DIR__.'/controllers/');
+$loader->addPsr4('Jonathan\\models\\', __DIR__.'/models/');
+$loader->addPsr4('Jonathan\\views\\', __DIR__.'/views/');
