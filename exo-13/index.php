bd89b14f7a9f006e32329d3ff84e640bb2547805
JJonathan <jolivald.jonathan@gmail.com>
2020-04-14 16:48:34 +0200

    add DB credentials

--- exo-13/index.php
+++ exo-13/index.php
@@ -8,3 +8,7 @@ $loader->addPsr4('Jonathan\\classes\\', __DIR__.'/classes/');
 $loader->addPsr4('Jonathan\\controllers\\', __DIR__.'/controllers/');
 $loader->addPsr4('Jonathan\\models\\', __DIR__.'/models/');
 $loader->addPsr4('Jonathan\\views\\', __DIR__.'/views/');
+
+// charge les infos sensibles depuis le fichier .env
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
