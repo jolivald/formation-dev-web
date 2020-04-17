7a42b5831931a3db4567275ee9de8a4c001874d0
JJonathan <jolivald.jonathan@gmail.com>
2020-04-17 13:49:39 +0200

    improve view contract

--- exo-13/views/IView.php
+++ exo-13/views/IView.php
@@ -4,6 +4,8 @@ namespace Jonathan\Views;
 
 interface IView {
 
+  public function __construct();
+  
   public function render() : string;
 
 }
--- exo-13/views/Login.php
+++ exo-13/views/Login.php
@@ -4,6 +4,8 @@ namespace Jonathan\Views;
 
 class Login implements IView {
 
+  public function __construct() {}
+
   public function render() : string {
     return 'login view';
   }
