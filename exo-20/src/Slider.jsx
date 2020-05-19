663a3e76de4d9f3982f6247b4353ff28dc315142
JJonathan <jolivald.jonathan@gmail.com>
2020-05-19 15:58:22 +0200

    display hec color

--- exo-20/src/Slider.jsx
+++ exo-20/src/Slider.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 
 const Slider = ({ label, value, onChange }) => {
   const id = 'slider-'+label.toLowerCase();
+  const dispatch = event =>  onChange(event.target.value);
   return (
     <div className="slider">
       <label htmlFor={id}>{label}</label>
       <input type="range" min="0" max="255" step="1"
         {...{ id, value }}
-        onChange={event => onChange(event.target.value)}
+        onChange={dispatch}
       />
-      <span>{value}</span>
+      <input type="number" min="0" max="255" step="1" size="3"
+        value={value}
+        onChange={dispatch}
+      />
     </div>
   );
 };
