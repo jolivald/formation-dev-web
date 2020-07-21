cb2d6bb0c3f682df73bfd18b48a7f3696752d1d1
JJonathan <jolivald.jonathan@gmail.com>
2020-07-21 14:54:21 +0200

    return json response

--- exo-24/src/Controller/ApiController.php
+++ exo-24/src/Controller/ApiController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
+// use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
@@ -19,14 +19,7 @@ class ApiController extends AbstractController
     public function index(PostRepository $postRepository, NormalizerInterface $normalizer)
     {
         $posts = $postRepository->findAll();
-        
         $post_normalize = $normalizer->normalize($posts);
-        //$json = json_encode($post_normalize);
-        //echo $json;
-        // return new JsonResponse($post_normalize);
-        return $this->render('api/index.html.twig', [
-            'controller_name' => 'ApiController',
-            'posts' => $posts
-        ]);
+        return new JsonResponse($post_normalize);
     }
 }
